5991d5631f02134e6d492fb9ed1573a4da296813
Blake Erquiaga <cbcerquiaga@me.com>
2019-03-18 15:58:00 -0500

    Init
    Initialized repo

---
+++ Team Kookaburra Chess/Team Kookaburra ChessTests/Team_Kookaburra_ChessTests.swift
@@ -0,0 +1,34 @@
+//
+//  Team_Kookaburra_ChessTests.swift
+//  Team Kookaburra ChessTests
+//
+//  Created by Christopher Blake Cassell Erquiaga on 3/18/19.
+//  Copyright © 2019 Christopher Blake Cassell Erquiaga. All rights reserved.
+//
+
+import XCTest
+@testable import Team_Kookaburra_Chess
+
+class Team_Kookaburra_ChessTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
