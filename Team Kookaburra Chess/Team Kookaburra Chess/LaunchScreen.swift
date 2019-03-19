63b11033311c916b44d30dc6b6befa7bb8e368de
MeghanStovall <meghanstovall15@augustana.edu>
2019-03-19 14:19:02 -0500

    Added Launch Screen

---
+++ Team Kookaburra Chess/Team Kookaburra Chess/LaunchScreen.swift
@@ -0,0 +1,9 @@
+//
+//  LaunchScreen.swift
+//  Team Kookaburra Chess
+//
+//  Created by Meghan Stovall on 3/19/19.
+//  Copyright © 2019 Christopher Blake Cassell Erquiaga. All rights reserved.
+//
+
+import Foundation
