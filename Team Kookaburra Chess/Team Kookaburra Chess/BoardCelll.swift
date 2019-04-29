0d07399574d402556333f6d56f31e195c5e8cd26
Blake Erquiaga <cbcerquiaga@me.com>
2019-04-29 09:41:40 -0500

    Revert "Players can't select empty spaces anymore"
    This reverts commit f3bbeba65035e8c7f30a71a9fb7c45967907d2dd.

--- Team Kookaburra Chess/Team Kookaburra Chess/BoardCelll.swift
+++ Team Kookaburra Chess/Team Kookaburra Chess/BoardCelll.swift
@@ -101,9 +101,6 @@ class BoardCell: UIView {
         row = piece.row
         column = piece.col
         self.piece = piece
-        if piece.type == .dummy{
-            piece.color = .clear
-        }
         //pieceLabel.attributedText = imageFromPath(path: "testRook.png")
         //special thanks to StackOverflow user Tarun Seera
         //Create Attachment
